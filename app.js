68fdb61913bd1465b192b500ac3efc3a1c17886a
Jiaxing Li <ljxerena39@gmail.com>
2018-02-08 22:23:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,29 +11,29 @@ app.get('/', function (req, res) {
 });
 
 var connectedSockets={};
-var allUsers=[{nickname:"",color:"#000"}];//初始值即包含"群聊",用""表示nickname
+var allUsers=[{nickname:"",color:"#000"}];
 io.on('connection',function(socket){
 
 
-    socket.on('addUser',function(data){ //有新用户进入聊天室
-        if(connectedSockets[data.nickname]){//昵称已被占用
+    socket.on('addUser',function(data){ 
+        if(connectedSockets[data.nickname]){
           socket.emit('userAddingResult',{result:false});
         }else{
             socket.emit('userAddingResult',{result:true});
             socket.nickname=data.nickname;
-            connectedSockets[socket.nickname]=socket;//保存每个socket实例,发私信需要用
+            connectedSockets[socket.nickname]=socket;
             allUsers.push(data);
-            socket.broadcast.emit('userAdded',data);//广播欢迎新用户,除新用户外都可看到
-            socket.emit('allUser',allUsers);//将所有在线用户发给新用户
+            socket.broadcast.emit('userAdded',data);
+            socket.emit('allUser',allUsers);
         }
 
     });
 
-    socket.on('addMessage',function(data){ //有用户发送新消息
-        if(data.to){//发给特定用户
+    socket.on('addMessage',function(data){ 
+        if(data.to){
             connectedSockets[data.to].emit('messageAdded',data);
-        }else{//群发
-            socket.broadcast.emit('messageAdded',data);//广播消息,除原发送者外都可看到
+        }else{
+            socket.broadcast.emit('messageAdded',data);
         }
 
 
@@ -41,8 +41,8 @@ io.on('connection',function(socket){
 
 
 
-    socket.on('disconnect', function () {  //有用户退出聊天室
-            socket.broadcast.emit('userRemoved', {  //广播有用户退出
+    socket.on('disconnect', function () {  
+            socket.broadcast.emit('userRemoved', {  
                 nickname: socket.nickname
             });
             for(var i=0;i<allUsers.length;i++){
@@ -50,7 +50,7 @@ io.on('connection',function(socket){
                     allUsers.splice(i,1);
                 }
             }
-            delete connectedSockets[socket.nickname]; //删除对应的socket实例
+            delete connectedSockets[socket.nickname]; 
 
         }
     );
